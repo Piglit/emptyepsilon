6031a39ab6260a55c19afebbf905ea776999a81c
Pithlit <piglit@mail.de>
2020-07-07 01:00:20 +1200

    removed melon

--- scripts/shipTemplates_special.lua
+++ scripts/shipTemplates_special.lua
@@ -50,67 +50,67 @@ template:addDoor(8, 4, false);
 
 
 --[[Corvette Melonidas--]]
-template = ShipTemplate():setName("Melonidas"):setClass("Corvette", "Destroyer"):setModel("AtlasDreadnoughtGreen"):setType("playership")
-template:setDescription([[The Melonidas is the smallest model of destroyer, and its combination of frigate-like size and corvette-like power makes it an excellent escort ship when defending larger ships against multiple smaller enemies. Because the Melonidas is fitted with a jump drive, it can also serve as an intersystem patrol craft.]])
-template:setRadarTrace("radar_melon.png")
-template:setJumpDrive(true)
-template:setShields(200, 200)
-template:setHull(250)
-template:setSpeed(90, 10, 20)
-template:setCombatManeuver(400, 250)
-template:setDockClasses("Starfighter")
---                  Arc, Dir, Range, CycleTime, Dmg
-template:setBeam(0,80, -80, 1500.0, 6.0, 8)
-template:setBeam(1,80,  80, 1500.0, 6.0, 8)
-template:setBeam(2,80, -100, 1500.0, 6.0, 8)
-template:setBeam(3,80,  100, 1500.0, 6.0, 8)
-template:setWeaponStorage("Homing", 12)
-template:setWeaponStorage("Nuke", 4)
-template:setWeaponStorage("Mine", 8)
-template:setWeaponStorage("EMP", 6)
-template:setWeaponStorage("HVLI", 20)
-template:setTubes(4, 8.0) -- Amount of torpedo tubes, and loading time of the tubes.
-template:weaponTubeDisallowMissle(0, "Mine")
-template:weaponTubeDisallowMissle(1, "Mine")
-template:weaponTubeDisallowMissle(2, "Mine")
-template:weaponTubeDisallowMissle(3, "Mine")
-
-template:setTubeDirection(0, 0)
-template:setTubeDirection(1, 0):weaponTubeDisallowMissle(1, "Nuke"):weaponTubeDisallowMissle(1, "EMP")
-template:setTubeDirection(2, 0):weaponTubeDisallowMissle(2, "Nuke"):weaponTubeDisallowMissle(2, "EMP")
-template:setTubeDirection(3, 180):setWeaponTubeExclusiveFor(3, "Mine")
-
-template:addRoomSystem(1, 0, 3, 1, "Maneuver");
-template:addRoom(0, 1, 2, 1);
-template:addRoom(2, 1, 1, 1);
-template:addRoom(3, 1, 2, 1);
-template:addRoom(2, 2, 2, 1);
-template:addRoomSystem(6, 2, 2, 1, "MissileSystem");
-template:addRoomSystem(1, 3, 1, 2, "RearShield");
-template:addRoomSystem(2, 3, 2, 2, "Reactor");
-template:addRoomSystem(4, 3, 2, 2, "JumpDrive");
-template:addRoomSystem(6, 3, 2, 2, "Warp");
-template:addRoomSystem(8, 3, 1, 2, "FrontShield");
-template:addRoom(2, 5, 2, 1);
-template:addRoomSystem(6, 5, 2, 1, "BeamWeapons");
-template:addRoom(0, 6, 2, 1);
-template:addRoom(2, 6, 1, 1);
-template:addRoom(3, 6, 2, 1);
-template:addRoomSystem(1, 7, 3, 1, "Impulse");
-
-template:addDoor(2, 1, true);
-template:addDoor(2, 1, false);
-template:addDoor(3, 1, false);
-template:addDoor(2, 2, true); 
-template:addDoor(3, 3, true);
-template:addDoor(3, 5, true);
-template:addDoor(2, 6, true);
-template:addDoor(2, 6, false);
-template:addDoor(3, 6, false);
-template:addDoor(2, 7, true); 
-template:addDoor(2, 3, false);
-template:addDoor(4, 4, false);
-template:addDoor(6, 3, false);
-template:addDoor(8, 4, false);
-template:addDoor(7, 3, true);
-template:addDoor(7, 5, true);
+--template = ShipTemplate():setName("Melonidas"):setClass("Corvette", "Destroyer"):setModel("AtlasDreadnoughtGreen"):setType("playership")
+--template:setDescription([[The Melonidas is the smallest model of destroyer, and its combination of frigate-like size and corvette-like power makes it an excellent escort ship when defending larger ships against multiple smaller enemies. Because the Melonidas is fitted with a jump drive, it can also serve as an intersystem patrol craft.]])
+--template:setRadarTrace("radar_melon.png")
+--template:setJumpDrive(true)
+--template:setShields(200, 200)
+--template:setHull(250)
+--template:setSpeed(90, 10, 20)
+--template:setCombatManeuver(400, 250)
+--template:setDockClasses("Starfighter")
+----                  Arc, Dir, Range, CycleTime, Dmg
+--template:setBeam(0,80, -80, 1500.0, 6.0, 8)
+--template:setBeam(1,80,  80, 1500.0, 6.0, 8)
+--template:setBeam(2,80, -100, 1500.0, 6.0, 8)
+--template:setBeam(3,80,  100, 1500.0, 6.0, 8)
+--template:setWeaponStorage("Homing", 12)
+--template:setWeaponStorage("Nuke", 4)
+--template:setWeaponStorage("Mine", 8)
+--template:setWeaponStorage("EMP", 6)
+--template:setWeaponStorage("HVLI", 20)
+--template:setTubes(4, 8.0) -- Amount of torpedo tubes, and loading time of the tubes.
+--template:weaponTubeDisallowMissle(0, "Mine")
+--template:weaponTubeDisallowMissle(1, "Mine")
+--template:weaponTubeDisallowMissle(2, "Mine")
+--template:weaponTubeDisallowMissle(3, "Mine")
+--
+--template:setTubeDirection(0, 0)
+--template:setTubeDirection(1, 0):weaponTubeDisallowMissle(1, "Nuke"):weaponTubeDisallowMissle(1, "EMP")
+--template:setTubeDirection(2, 0):weaponTubeDisallowMissle(2, "Nuke"):weaponTubeDisallowMissle(2, "EMP")
+--template:setTubeDirection(3, 180):setWeaponTubeExclusiveFor(3, "Mine")
+--
+--template:addRoomSystem(1, 0, 3, 1, "Maneuver");
+--template:addRoom(0, 1, 2, 1);
+--template:addRoom(2, 1, 1, 1);
+--template:addRoom(3, 1, 2, 1);
+--template:addRoom(2, 2, 2, 1);
+--template:addRoomSystem(6, 2, 2, 1, "MissileSystem");
+--template:addRoomSystem(1, 3, 1, 2, "RearShield");
+--template:addRoomSystem(2, 3, 2, 2, "Reactor");
+--template:addRoomSystem(4, 3, 2, 2, "JumpDrive");
+--template:addRoomSystem(6, 3, 2, 2, "Warp");
+--template:addRoomSystem(8, 3, 1, 2, "FrontShield");
+--template:addRoom(2, 5, 2, 1);
+--template:addRoomSystem(6, 5, 2, 1, "BeamWeapons");
+--template:addRoom(0, 6, 2, 1);
+--template:addRoom(2, 6, 1, 1);
+--template:addRoom(3, 6, 2, 1);
+--template:addRoomSystem(1, 7, 3, 1, "Impulse");
+--
+--template:addDoor(2, 1, true);
+--template:addDoor(2, 1, false);
+--template:addDoor(3, 1, false);
+--template:addDoor(2, 2, true); 
+--template:addDoor(3, 3, true);
+--template:addDoor(3, 5, true);
+--template:addDoor(2, 6, true);
+--template:addDoor(2, 6, false);
+--template:addDoor(3, 6, false);
+--template:addDoor(2, 7, true); 
+--template:addDoor(2, 3, false);
+--template:addDoor(4, 4, false);
+--template:addDoor(6, 3, false);
+--template:addDoor(8, 4, false);
+--template:addDoor(7, 3, true);
+--template:addDoor(7, 5, true);
